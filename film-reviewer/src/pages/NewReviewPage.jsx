6b22af8d95c2192280dd4c7a3470c64729808328
oscarigu <38469752+Oscarigu@users.noreply.github.com>
2025-10-08 00:23:10 +0200

    form

--- film-reviewer/src/pages/NewReviewPage.jsx
+++ film-reviewer/src/pages/NewReviewPage.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "@mantine/form";
+import { TextInput, Button, Group } from "@mantine/core";
+import { randomId } from "@mantine/hooks";
 
-const NewReviewPage = () => (
-    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
-        <h1>404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <Link to="/">Go back to Home</Link>
+function NewReviewPage() {
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      name: "",
+      email: "",
+    },
+  });
+
+  return (
+    <div>
+      <TextInput
+        label="Name"
+        placeholder="Name"
+        key={form.key("name")}
+        {...form.getInputProps("name")}
+      />
+
+      <TextInput
+        mt="md"
+        label="Email"
+        placeholder="Email"
+        key={form.key("email")}
+        {...form.getInputProps("email")}
+      />
+
+      <Group justify="center" mt="xl">
+        <Button
+          onClick={() =>
+            form.setValues({
+              name: randomId(),
+              email: `${randomId()}@test.com`,
+            })
+          }
+        >
+          Set random values
+        </Button>
+      </Group>
+
+      <Group justify="center" mt="md">
+        <Button component={Link} to="/">
+          Back to Home
+        </Button>
+      </Group>
     </div>
-);
+  );
+}
 
 export default NewReviewPage;
