6ed5847be22ab344389965242bb3f37146bc2f5c
oscarigu <38469752+Oscarigu@users.noreply.github.com>
2025-10-08 10:21:47 +0200

    fallo mantine resuelto, ultima version

--- film-reviewer/src/main.jsx
+++ film-reviewer/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { MantineProvider } from '@mantine/core';
 import './index.css'
 import App from './App.jsx'
+import '@mantine/core/styles.css'
 
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -12,7 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Router>
         <App />
       </Router>
-    </StrictMode>,
+    </StrictMode>
   </MantineProvider>
 )
 
